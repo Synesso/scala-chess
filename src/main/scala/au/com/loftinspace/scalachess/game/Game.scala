c3c927bd4a112dee4ba63fbea076ada7daf402dc
Jeremy Mawson <jem.mawson@gmail.com>
2009-04-29 22:25:22 +1000

    Quick refactor of Game

--- src/main/scala/au/com/loftinspace/scalachess/game/Game.scala
+++ src/main/scala/au/com/loftinspace/scalachess/game/Game.scala
@@ -10,8 +10,7 @@ class Game {
   def place(p: Piece) = new Placement(p)
   case class Placement(p: Piece) {
     def at(s: Symbol): Option[Piece] = {
-      validateCoordinate(s)
-      val index = arrayIndexForSymbol(s)
+      val index = arrayIndexForCoordinate(s)
       val oldPiece = pieces(index)
       val oldIndex = pieces.findIndexOf((anOption: Option[Piece]) => anOption equals Some(p))
       if (oldIndex >= 0) pieces(oldIndex) = None
@@ -20,13 +19,12 @@ class Game {
     }
   }
 
-  def pieceAt(s: Symbol):Option[Piece] = {
-    validateCoordinate(s)
-    pieces(arrayIndexForSymbol(s))
-  }
+  def pieceAt(s: Symbol):Option[Piece] = pieces(arrayIndexForCoordinate(s))
 
-  private def validateCoordinate(coord: Symbol) = if (coord == null || CoordinatePattern.findFirstIn(coord.name).equals(None)) throw new IllegalArgumentException("Invalid coordinate: " + coord)
-  private def arrayIndexForSymbol(s: Symbol): Int = ((s.name.charAt(0).asDigit - 10) * 8) + (s.name.charAt(1).asDigit - 1)
+  private def arrayIndexForCoordinate(s: Symbol): Int = {
+    if (s == null || CoordinatePattern.findFirstIn(s.name).equals(None)) throw new IllegalArgumentException("Invalid coordinate: " + s)
+    ((s.name.charAt(0).asDigit - 10) * 8) + (s.name.charAt(1).asDigit - 1)
+  }
 
   // todo - get rid of this!
   def includeIn_:[P <: Piece](piece: P): Unit = {}
