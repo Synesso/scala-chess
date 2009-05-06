ad4e4f3688cd1ec5205733098b5512304577dd4f
Jeremy Mawson <jem.mawson@gmail.com>
2009-05-06 20:24:45 +1000

    Removing redundant checks

--- src/main/scala/au/com/loftinspace/scalachess/game/Movements.scala
+++ src/main/scala/au/com/loftinspace/scalachess/game/Movements.scala
@@ -5,8 +5,6 @@ case class MovementQuery(p: Piece, fromOption: Option[Position], to: InPosition)
   def given(g: Game): Unit = {
     if (fromOption.isEmpty) return
     val from = fromOption.get
-    if (from.equals(Captured)) throw new IllegalMoveException("Captured pieces cannot be moved")
-    if (from equals to) throw new IllegalMoveException("Piece would not be moved")
     val movement = from >> to
     if (!movement.candidates.contains(p.role))
       throw new IllegalMoveException(p.role.toString + "s cannot move " + movement.description)
