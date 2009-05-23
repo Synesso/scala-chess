1f30c318d2ab2a25a4f386c2696e455c43227370
Jeremy Mawson <jem.mawson@gmail.com>
2009-05-23 21:45:51 +1000

    Position has -? |? and /? operators. En passant now returns the captured piece.

--- src/test/scala/au/com/loftinspace/scalachess/game/PositionAssertions.scala
+++
@@ -1,7 +0,0 @@
-package au.com.loftinspace.scalachess.game
-
-import org.specs.matcher._
-
-object PositionAssertions extends IterableMatchers {
-  def containPositions(s: Symbol*) = containAll(s.map(position(_)))
-}
